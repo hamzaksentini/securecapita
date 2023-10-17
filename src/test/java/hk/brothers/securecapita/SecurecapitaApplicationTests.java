2656e9e3aeda29f52b5a1e879f014a5dd4cf424c
hksentini <hksentini@talys.digital>
2023-10-17 17:33:01 +0100

    first commit

---
+++ src/test/java/hk/brothers/securecapita/SecurecapitaApplicationTests.java
@@ -0,0 +1,13 @@
+package hk.brothers.securecapita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecapitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
